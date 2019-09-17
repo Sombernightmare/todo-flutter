db8a142d5717aa760cd73bcf54091f39f9c32606
sombernightmare <timothytallen@gmail.com>
2019-09-16 23:24:06 -0500

    Add new task using Provider

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
-  List<Task> tasks = [Task(name: '1'), Task(name: '2'), Task(name: '3')];
+  List<Task> _tasks = [Task(name: '1'), Task(name: '2'), Task(name: '3')];
+
+  void addTask(String TaskTitle) {
+    final task = Task(name: TaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
 }
